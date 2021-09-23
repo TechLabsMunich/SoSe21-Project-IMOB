6ab3f6a7c827cd6c376f51ee86009223156d8b72
Miki <miki.cyg@gmail.com>
2021-09-23 22:04:50 +0200

    working on functions dealing with the fact the we changed concept of storing files and storing the target variables which are now located in seperate table not in folder names. Not finished.

--- 03-Code/Main.py
+++ 03-Code/Main.py
@@ -4,3 +4,4 @@ path = '/Users/Mikolaj/PycharmProjects/SoSe21-Project-IMOB/01-Data'
 X, y = f.create_X_y(path)
 print(X)
 print(y)
+
--- 03-Code/functions.py
+++ 03-Code/functions.py
@@ -3,11 +3,32 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import os
 
+# target_var_file_path = "../01-Data/new data's IDs.xlsx"
+def create_target_var_df(path):
+    """creates target var df from which one can later pick the target he wants and merge it to final df"""
+    data_id = pd.read_excel(path)
+    data_id.dropna(subset=['ID'], inplace=True)
+    # dealing with dumb P's
+    better_ids = []
+    for x in data_id['ID']:
+        l = list(x)
+        del l[0]
+        s = ('').join(l)
+        n = int(s)
+        better_ids.append(n)
+    data_id['ID'] = better_ids
+    # sorting the table
+    data_id.sort_values(by=['ID'], inplace=True)
+    data_id = data_id.reset_index()
+    data_id.drop(['index'], axis=1, inplace=True)
+
+    return data_id
+
 
 def _xls_to_df(path):
     """When provided with our .xls file produces a dataframe with only one row of lists in each column.
     Columns' names stay the same."""
-    #prepare old the starter dataframe
+    #prepare starter dataframe
     df = pd.read_excel(path)
     df.drop([0], inplace=True)
     df.reset_index(inplace=True)
@@ -23,8 +44,23 @@ def _xls_to_df(path):
     df2.columns = df.columns
     #dropping the time column
     df2.drop(['Unnamed: 0'], axis =1, inplace =True)
-    return df2
 
+    #putting in ID column from name of the file
+    path_list = path.split('/')
+    filename = path_list[-1]
+    p= filename.split('.')
+    p_number = p[0]
+    l=list(p_number)
+    del l[0]
+    s = ('').join(l)
+    n=int(s)
+    new_columns = ['ID']
+    for a in df2.columns:
+        new_columns.append(a)
+    df2['ID'] = n
+    df2=df2.reindex(columns = new_columns)
+
+    return df2
 
 
 def make_dataframes(directory_path):
@@ -33,28 +69,24 @@ def make_dataframes(directory_path):
     files_list = os.listdir(directory_path)
     files_list.sort()
     for filename in files_list:
-        #no idea why it does not work. gives OverflowError
-        try:
-            file_path = directory_path+'/'+filename
-            df = _xls_to_df(file_path)
-            dataframes.append(df)
-        except OverflowError:
-            continue
-        #print function just for debugging purposes to see which files are going through and which do not
-        # print(filename)
+        file_path = directory_path+'/'+filename
+        df = _xls_to_df(file_path)
+        dataframes.append(df)
     return dataframes
 
-def merge_dataframes(dataframes, target_var):
+
+def merge_dataframes(dataframes, target_var_df, target_var):
     """provided a list of dataframes and name of target variable creates one concated dataframe with additional column target"""
     new_df = pd.concat(dataframes, ignore_index = True)
-    new_df['Target'] = target_var
+    small_target_var_df = target_var_df[['ID', target_var]]
+    new_df = new_df.merge(small_targer_var_df, on='ID')
     return new_df
 
-def merge_sets(dataframes):
-    """concats dataframes from different drug groups"""
-    new_df = pd.concat(dataframes, ignore_index = True)
-    return new_df
 
+# def merge_sets(dataframes):
+#     """concats dataframes from different drug groups"""
+#     new_df = pd.concat(dataframes, ignore_index = True)
+#     return new_df
 
 
 def create_X_y(directory_path):
@@ -75,3 +107,6 @@ def create_X_y(directory_path):
     X = big_df.drop(['Target'], axis=1)
 
     return X, y
+
+z = make_dataframes('../01-Data/new_data')
+print(z)
