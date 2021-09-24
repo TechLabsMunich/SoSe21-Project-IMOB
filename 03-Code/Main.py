60f5875e111987ddaf0d09fbae268533dc77d7a2
Miki <miki.cyg@gmail.com>
2021-09-24 15:12:14 +0200

    started with machine learining part in the main file

--- 03-Code/Main.py
+++ 03-Code/Main.py
@@ -7,7 +7,21 @@ id_path = "../01-Data/new data's IDs.xlsx"
 target_variable = 'Ruhepuls'
 
 X, y = f.create_X_y(data_path, id_path, target_variable)
+print(X)
+print(y)
+
 
 #MACHINE LEARNING PART:
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 
+from sktime.classification.compose import ColumnEnsembleClassifier
+from sktime.classification.dictionary_based import BOSSEnsemble
+from sktime.classification.interval_based import TimeSeriesForestClassifier
+from sktime.classification.shapelet_based import MrSEQLClassifier
+from sktime.datasets import load_basic_motions
+from sktime.transformations.panel.compose import ColumnConcatenator
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
--- 03-Code/functions.py
+++ 03-Code/functions.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 import os
 
 def create_target_var_df(path):
